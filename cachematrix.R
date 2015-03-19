6b6fc5bc693e03166d1fbd2b36063f00f8bfc4dd
vatsalyamathur <vatsalya.mathur@gmail.com>
2015-03-19 20:26:12 +0530

    Final Commit
    Submission for Coursera R Programming.

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,13 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
 ## Write a short comment describing this function
-
+## makeCacheMatrix() defines a new kind of matrix which caches it's inverse
 makeCacheMatrix <- function(x = matrix()) {
-inverse <- NULL
+  inverse <- NULL
   if (!is.matrix(x))
   {
+    ## if x is not a matrix Object
+    ## exit and print appropriate message
     print("Enter Matrix Object as argument")
     return(NULL)
   }
@@ -21,26 +22,28 @@ inverse <- NULL
        setinverse = setinverse,
        getinverse = getinverse)
 }
-
-
 ## Write a short comment describing this function
-
+## cacheSolve() is used to cache the inverse value of the matrix defined through makeCacheMatrix()
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        dimMatrix<-dim(m$get())
+  ## Return a matrix that is the inverse of 'x'
+  dimMatrix<-dim(x$get())
   if (!identical(dimMatrix[1], dimMatrix[2]))
   {
     print("nrow != ncol")
     print("Matrix should be square")
     return(NULL)
   }
-  i <- m$getinverse()
+  i <- x$getinverse()
+  ## check for cached value for inverse matrix
   if(!is.null(i)) {
+  ## if cache value of inverse not null
     message("Getting Cached Data")
+  ## return cached inverse matrix
     return(i)
   }
-  data <- m$get()
+  data <- x$get()
   i <- solve(data, ...)
-  m$setinverse(i)
+  x$setinverse(i)
+  ## return inverse matrix
   i
 }
